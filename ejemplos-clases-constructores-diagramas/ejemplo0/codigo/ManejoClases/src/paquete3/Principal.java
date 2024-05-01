019d7c5ff4453c9b6424af3e68c48f2104ec301b
estoledo1 <estoledo1@utpl.edu.ec>
2024-05-01 16:31:04 -0500

    constructores01

---
+++ ejemplos-clases-constructores-diagramas/ejemplo0/codigo/ManejoClases/src/paquete3/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete3;
+
+/**
+ *
+ * @author utpl
+ */
+public class Principal {
+    public static void main(String[] args) {
+        // Al objeto que fue creado no tiene ningun constructor creado, asi que 
+        // se usa un constructor por defecto para tomar sus valores 
+        Computadora computadoratrabajo = new Computadora();
+    }
+}
